854e09f0d3e2ff2a29c36ec5847d667c8ac95378
Vincent <Cephalobyte@users.noreply.github.com>
2021-10-05 12:45:33 -0400

    optimisation js & deplacer variables.scss

--- csf-squishystudios-vite/main.js
+++ csf-squishystudios-vite/main.js
@@ -5,16 +5,17 @@ const middle = document.querySelector('middle');
 const lower = document.querySelector('.lower');
 const hamburger = document.querySelector('.hamburger')
 
-hamburger.addEventListener('click', function() {
-  var x = document.querySelector('.hamburger-toggle');
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-  });
+hamburger.addEventListener('click', ()=> {
+	var x = document.querySelector('.hamburger-toggle');
+
+	if (x.style.display == "block") {
+		x.style.display = "none";
+		return;
+	}
+	x.style.display = "block";
+});
 
 /*gsap.timeline()
-  .to(upper, 0.5, {attr: {d: "M8,2 L2,8"}, x: 1, ease:Power2.easeInOut}, 'start')
-  .to(middle, 0.5, {autoAlpha: 0}, 'start')
-  .to(lower, 0.5, {attr: {d: "M8,8 L2,2"}, x: 1, ease:Power2.easeInOut}, 'start');*/
+	.to(upper, 0.5, {attr: {d: "M8,2 L2,8"}, x: 1, ease:Power2.easeInOut}, 'start')
+	.to(middle, 0.5, {autoAlpha: 0}, 'start')
+	.to(lower, 0.5, {attr: {d: "M8,8 L2,2"}, x: 1, ease:Power2.easeInOut}, 'start');*/

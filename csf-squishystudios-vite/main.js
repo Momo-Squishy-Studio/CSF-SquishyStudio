cac75d45a77f1ddffc8139315ee64aba52b4b2c7
MaxiS92 <max.sab@hotmail.fr>
2021-10-04 22:57:31 -0400

    menu-hamburger-nav

--- csf-squishystudios-vite/main.js
+++ csf-squishystudios-vite/main.js
@@ -1,13 +1,20 @@
 import './style.scss'
 
-var upper = document.getElementsByClassName('upper');
-var middle = document.getElementsByClassName('middle');
-var lower = document.getElementsByClassName('lower');
-var hamburger = document.getElementsByClassName('hamburger')
+const upper = document.querySelector('.upper');
+const middle = document.querySelector('middle');
+const lower = document.querySelector('.lower');
+const hamburger = document.querySelector('.hamburger')
 
 hamburger.addEventListener('click', function() {
-    gsap.timeline()
-    .to(upper, 0.5, {attr: {d: "M8,2 L2,8"}, x: 1, ease:Power2.easeInOut}, 'start')
-    .to(middle, 0.5, {autoAlpha: 0}, 'start')
-    .to(lower, 0.5, {attr: {d: "M8,8 L2,2"}, x: 1, ease:Power2.easeInOut}, 'start');
+  var x = document.querySelector('.hamburger-toggle');
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
   });
+
+/*gsap.timeline()
+  .to(upper, 0.5, {attr: {d: "M8,2 L2,8"}, x: 1, ease:Power2.easeInOut}, 'start')
+  .to(middle, 0.5, {autoAlpha: 0}, 'start')
+  .to(lower, 0.5, {attr: {d: "M8,8 L2,2"}, x: 1, ease:Power2.easeInOut}, 'start');*/

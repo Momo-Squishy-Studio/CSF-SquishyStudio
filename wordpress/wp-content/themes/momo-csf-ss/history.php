bf0bf7020ce0e2132162dc03fa3d6105a593b4c2
Maxime Sabourin <70408582+MaxiS92@users.noreply.github.com>
2021-11-19 16:55:33 -0500

    hub service

--- wordpress/wp-content/themes/momo-csf-ss/history.php
+++ wordpress/wp-content/themes/momo-csf-ss/history.php
@@ -14,13 +14,8 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 
 	<?php get_template_part( 'partials/hero-generique' );?>
 
-<div style='height: 100px; background: url("<?php echo get_template_directory_uri(); ?>/site_ressources/images/vague.svg") center / 100% 100% no-repeat'></div>
-
 <?php get_template_part( 'partials/description' ); ?>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#EC1A24" fill-opacity="1" d="M0,160L80,181.3C160,203,320,245,480,234.7C640,224,800,160,960,160C1120,160,1280,224,1360,256L1440,288L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
-</path>
-</svg>
 
 		<?php endif; ?>
 		
